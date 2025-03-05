8d038e0928bcd5a2a4070fab6892c0ac12b9dafb
Huy Doan <doanvanhuydhdtu@gmail.com>
2025-03-05 15:22:28 +0700

    #10 Phân quyền (Authorization) với PreAuthorize và PostAuthorize

--- identity-service/src/main/java/com/hdoan/identity_service/configuration/SecurityConfig.java
+++ identity-service/src/main/java/com/hdoan/identity_service/configuration/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -25,6 +26,7 @@ import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
 
@@ -39,10 +41,6 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.authorizeHttpRequests(request ->
                 request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
-                        .requestMatchers(HttpMethod.GET, "/users")
-                        //.hasAuthority("ROLE_ADMIN")
-                        .hasRole(Role.ADMIN.name())
-                        
                     .anyRequest()
                     .authenticated()
         );
--- identity-service/src/main/java/com/hdoan/identity_service/service/UserService.java
+++ identity-service/src/main/java/com/hdoan/identity_service/service/UserService.java
@@ -15,17 +15,23 @@ import com.hdoan.identity_service.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
 public class UserService {
 
     UserRepository userRepository;
@@ -51,6 +57,16 @@ public class UserService {
         return userMapper.toUserResponse(userRepository.save(user));
     }
 
+    public UserResponse getMyInfo(){
+        var context = SecurityContextHolder.getContext();
+        String name = context.getAuthentication().getName();
+
+        User user = userRepository.findByUsername(name).orElseThrow(
+                () -> new AppException(ErrorCode.USER_NOT_EXISTED));
+
+        return userMapper.toUserResponse(user);
+    }
+
     public UserResponse updateUser(String userId, UserUpdateRequest request){
         User user = userRepository.findById(userId)
                 .orElseThrow(
@@ -66,18 +82,23 @@ public class UserService {
         userRepository.deleteById(userId);
     }
 
+    // Kiem tra dieu kien xong moi vao ben trong ham thuc thi
+    @PreAuthorize("hasRole('ADMIN')")
     public List<UserResponse> getUsers(){
+        log.info("In method get Users");
         return userRepository.findAll().stream()
                 .map(userMapper::toUserResponse)
                 .toList();
     }
 
+    // Thuc thi trong ham xong moi kiem tra roles
+    @PostAuthorize("returnObject.username == authentication.name")
     public UserResponse getUser(String id){
+        log.info("In method get user by id");
         return userMapper.toUserResponse(userRepository.findById(id)
                 .orElseThrow(
-                () -> new RuntimeException("User not found")
+                () -> new AppException(ErrorCode.USER_NOT_EXISTED)
         ));
-
     }
 
 }
